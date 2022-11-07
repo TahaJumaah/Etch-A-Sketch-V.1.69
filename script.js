8d3ec1f28e94f5432b9c87cf907f7c0cd60cc6fa
Taha Jumaah <taharasheed95@gmail.com>
2022-11-07 17:10:09 +0100

    most of the layout is done, working out the kinks of the event listener.

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 let pixelHeight = '0vh'
 let pixelWidth = ' 0vw'
 let index = 1;
-let smalCanvasButton = document.getElementById('smallCanvas');
+let clickedDiv = document.getElementsByClassName('drawingDiv');
+let mainDrawingCanvas = document.getElementById('drawingContainerCentered');
+let smallCanvasButton = document.getElementById('smallCanvas');
+let mediumCanvasButon = document.getElementById('mediumCanvas');
+let largeCanvasButton = document.getElementById('largeCanvas');
 
 
+
+function clearCanvas() {
+    let mainCanvas = document.getElementById('drawingContainerCentered')
+    mainCanvas.innerHTML='';
+}
 // Canvas Types, attached to buttons, all they do is set the size, then loop the actual div creation function.
 
 function smallCanvas() {
+    clearCanvas();
 pixelHeight = '10%';
 pixelWidth = '10%';
+index = 1;
 for (index ; index <= 100; index++) {
-
     createPixels();    
 }   
 }
 
 function mediumCanvas() {
-    pixelHeight = '5%';
-    pixelWidth = '5%';
+    clearCanvas();
+    pixelHeight = '6.5%';
+    pixelWidth = '5.5%';
     index = 1;
-    for (index ; index <= 400; index++) {
+    for (index ; index <= 270; index++) {
         createPixels();
     }}
 
     function largeCanvas() {
-        pixelHeight = '2.5%';
-        pixelWidth = '2.5%'
+        clearCanvas();
+        pixelHeight = '5%';
+        pixelWidth = '4%'
         index = 1;
-        for (index; index < 2000; index++) {
+        for (index; index < 501; index++) {
             createPixels()
             
         }
@@ -35,16 +47,33 @@ function mediumCanvas() {
     }
 //END OF CANVAS TYPES
 
+// function addColor() {
+//     drawingDiv.style.backgroundColor = 'red';
+    
+// };
 
 function createPixels(numberOfPixels){
-    let div = document.createElement('div');
-    div.style.width = pixelHeight;
-    div.style.height = pixelWidth;
-    div.style.backgroundColor = 'red';
-    document.getElementById('drawingContainerCentered').appendChild(div);
-
+    let drawingDiv = document.createElement('div');
+    drawingDiv.style.width = pixelHeight;
+    drawingDiv.style.height = pixelWidth;
+    drawingDiv.className = 'drawingDiv'
+    // div.style.backgroundColor = 'red';
+    drawingDiv.style.maxheight = 'inherit';
+    // div.style.border = '0.1px solid black'
+    document.getElementById('drawingContainerCentered').appendChild(drawingDiv);
+    // addColor();
+    // mainDrawingCanvas.style.width = 'fit-content';
 }
 
-smalCanvasButton.addEventListener('click'
+// function addColor(){
+//     let divToColor = document.getElementsByClassName('drawingDiv');
+//     divToColor.addEventListener('click', addColor());
+//     divToColor.style.backgroundColor = 'red';
+// }
 
 
+
+
+smallCanvasButton.addEventListener('click', smallCanvas)
+mediumCanvasButon.addEventListener('click', mediumCanvas)
+largeCanvasButton.addEventListener('click', largeCanvas)
